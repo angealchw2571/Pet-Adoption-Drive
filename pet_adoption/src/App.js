ef73a10502bb7daf32c619282971e5cec45f80a8
angeal2571 <angealty96@gmail.com>
2021-10-07 20:18:30 +0800

    Initial Commit

---
+++ pet_adoption/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+
+function App() {
+  console.log(process.env.REACT_APP_API_KEY)
+  return (
+    <div>
+      <h1>test</h1>
+      
+    </div>
+  );
+}
+
+export default App;
