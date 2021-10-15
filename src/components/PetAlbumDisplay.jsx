aaeb2ce656394de7d8843fc45ee2e4bfaefa6f5a
angealchw2571 <angealty96@gmail.com>
2021-10-15 11:37:20 +0800

    deleted stuff

--- src/components/PetAlbumDisplay.jsx
+++ src/components/PetAlbumDisplay.jsx
@@ -4,7 +4,6 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
